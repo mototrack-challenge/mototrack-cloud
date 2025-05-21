cffd672db9822c9d6f6eb6e304c5cbcc5445c4f9
felipesora <felipeusora@gmail.com>
2025-05-20 23:55:28 -0300

    Atualizando os scripts

--- instalar-docker-git.sh
+++ instalar-docker-git.sh
@@ -16,10 +16,7 @@ ssh $USERNAME@$VM_IP << 'EOF'
 
   # Adicionar repositório do Docker
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
   # Instalar Docker
   sudo apt update -y
