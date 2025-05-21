542133cd75dc334e6402cde21f1ab224e59d5d88
felipesora <felipeusora@gmail.com>
2025-05-21 19:40:23 -0300

    arruamando scripts

--- criar-vm-mototrack.sh
+++ criar-vm-mototrack.sh
@@ -9,7 +9,14 @@ PASSWORD="FIAP@2tdspy#"
 az group create --name $RESOURCE_GROUP --location $LOCATION
 
 # Criar VM com imagem Ubuntu LTS
-az vm create --resource-group $RESOURCE_GROUP --name $VM_NAME --image Ubuntu2204 --size Standard_B2s --admin-username $USERNAME --admin-password "$PASSWORD" --authentication-type password
+az vm create \
+  --resource-group $RESOURCE_GROUP \
+  --name $VM_NAME \
+  --image Ubuntu2204 \
+  --size Standard_B2s \
+  --admin-username $USERNAME \
+  --admin-password "$PASSWORD" \
+  --authentication-type password
 
 # Abrir a porta 8080
 az vm open-port --port 8080 --resource-group $RESOURCE_GROUP --name $VM_NAME
